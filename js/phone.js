48584d370d49bb3c053900f2cdf6b37135c8a830
unknown <jasimuddin25635@gmail.com>
2022-03-01 01:03:54 +0600

    added API

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,29 @@
-console.log("phone");
+const searchPhone = () => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => phoneDetail(data.data));
+};
+// searchPhone();
+const phoneDetail = (phones) => {
+  //   console.log(phones);
+  const detailDiv = document.getElementById("search-result");
+  phones.forEach((phone) => {
+    console.log(phone);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="col">
+          <div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p class="card-text">
+                ${phone.brand}
+              </p>
+            </div>
+          </div>
+        </div>
+    `;
+    detailDiv.appendChild(div);
+  });
+};
