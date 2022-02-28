2fb606dc484cf01e5235829951f00ea8af1e81e6
unknown <jasimuddin25635@gmail.com>
2022-03-01 02:34:39 +0600

    added detail button

--- js/phone.js
+++ js/phone.js
@@ -1,29 +1,44 @@
 const searchPhone = () => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+  const searchField = document.getElementById("search-phone");
+  //   console.log(searchField.value);
+  const searchText = searchField.value;
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  //   console.log(url);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => phoneDetail(data.data));
+    .then((data) => displayPhone(data.data));
+  searchField.value = "";
 };
 // searchPhone();
-const phoneDetail = (phones) => {
+const displayPhone = (phones) => {
   //   console.log(phones);
   const detailDiv = document.getElementById("search-result");
   phones.forEach((phone) => {
     console.log(phone);
     const div = document.createElement("div");
     div.innerHTML = `
-    <div class="col">
-          <div class="card">
-            <img src="${phone.image}" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">${phone.phone_name}</h5>
-              <p class="card-text">
-                ${phone.brand}
-              </p>
+      <div class="col">
+            <div class="card">
+              <img src="${phone.image}" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title text-center">${phone.brand}</h5>
+                <p class="card-text text-center">
+                  ${phone.phone_name}
+                </p>
+                <button type="button" class="btn btn-primary w-100" onclick="phoneDetails('detail')">Details</button>
+              </div>
+
             </div>
-          </div>
         </div>
-    `;
+      `;
     detailDiv.appendChild(div);
   });
+};
+const phoneDetails = (detail) => {
+  //   console.log("ddd");
+  const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+  //   console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 };
