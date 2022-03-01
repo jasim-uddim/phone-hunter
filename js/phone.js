f4ea4a1863a0d60aa7ecfdfc50711e15c0f24c4d
unknown <jasimuddin25635@gmail.com>
2022-03-01 15:17:50 +0600

    added error hendal

--- js/phone.js
+++ js/phone.js
@@ -1,6 +1,11 @@
+const phoneDetailsDiv = document.getElementById("phone-details");
+const searchField = document.getElementById("search-phone");
+const noResultDiv = document.getElementById("no-result");
+const resultDiv = document.getElementById("result");
+const detailDiv = document.getElementById("search-result");
 const searchPhone = () => {
-  const searchField = document.getElementById("search-phone");
-  //   console.log(searchField.value);
+  phoneDetailsDiv.textContent = "";
+
   const searchText = searchField.value;
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   //   console.log(url);
@@ -9,49 +14,61 @@ const searchPhone = () => {
     .then((data) => displayPhone(data.data));
   searchField.value = "";
 };
-// searchPhone();
-const displayPhone = (phones) => {
-  //   console.log(phones);
-  const detailDiv = document.getElementById("search-result");
-  phones.forEach((phone) => {
-    const div = document.createElement("div");
-    div.innerHTML = `
-      <div class="col">
-            <div class="card">
-              <img src="${phone.image}" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title text-center">${phone.brand}</h5>
-                <p class="card-text text-center">
-                  ${phone.phone_name}
-                </p>
-                <button type="button" class="btn btn-primary w-100" onclick="phoneDetails('${phone.slug}')">Details</button>
-              </div>
 
-            </div>
-        </div>
-      `;
-    detailDiv.appendChild(div);
-  });
+const displayPhone = (phones) => {
+  console.log(phones.length);
+
+  detailDiv.textContent = "";
+  if (phones.length == 0) {
+    const p = document.createElement("p");
+    p.innerText = "not fount result";
+    noResultDiv.appendChild(p);
+  } else {
+    noResultDiv.textContent = "";
+    phones.forEach((phone) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <div class="col">
+              <div class="card">
+                <img src="${phone?.image}" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title text-center">${phone?.brand}</h5>
+                  <p class="card-text text-center">
+                    ${phone?.phone_name}
+                  </p>
+                  <button type="button" class="btn btn-primary w-100" onclick="phoneDetails('${phone?.slug}')">Details</button>
+                </div>
+  
+              </div>
+          </div>
+        `;
+      const p = document.createElement("p");
+      p.innerText = `result:${phones.length}`;
+      resultDiv.appendChild(p);
+      detailDiv.appendChild(div);
+    });
+  }
 };
+// single phone Details
 const phoneDetails = (detail) => {
   // console.log(detail);
 
   const url = `https://openapi.programming-hero.com/api/phone/${detail}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayPhoneDetails(data.data));
+    .then((data) => displayPhoneDetails(data?.data));
 };
 const displayPhoneDetails = (data) => {
   console.log(data);
-  const phoneDetailsDiv = document.getElementById("phone-details");
+  phoneDetailsDiv.textContent = "";
   const div = document.createElement("div");
   div.innerHTML = `
   <div class="card w-50 mx-auto mb-4 p-4">
-    <img src="${data.image}" class="card-img-top" alt="..." />
+    <img src="${data?.image}" class="card-img-top" alt="..." />
     <div class="card-body">
-        <h5 class="card-title text-center">${data.brand}</h5>
+        <h5 class="card-title text-center">${data?.brand}</h5>
         <p class="card-text text-center">
-        ${data.name}
+        ${data?.name}
         </p>
       </div>
   </div>       
@@ -63,10 +80,20 @@ const displayPhoneDetails = (data) => {
     </div>
     <div class="row gx-0">
       <div class="col-4">
+      <div class="border bg-light px-2">releaseDate</div>
+      </div>
+      <div class="col-8">
+        <div class=" border bg-light px-2">${
+          data.releaseDate ? data.releaseDate : "no fount release date"
+        }</div>
+      </div>
+    </div>
+    <div class="row gx-0">
+      <div class="col-4">
       <div class="border bg-light px-2">brand</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.brand}</div>
+        <div class=" border bg-light px-2">${data?.brand}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -74,7 +101,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">name</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.name}</div>
+        <div class=" border bg-light px-2">${data?.name}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -82,7 +109,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">slug</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.slug}</div>
+        <div class=" border bg-light px-2">${data?.slug}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -95,7 +122,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">chipSet</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.mainFeatures.chipSet}</div>
+        <div class=" border bg-light px-2">${data?.mainFeatures?.chipSet}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -103,7 +130,9 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">displaySize</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.mainFeatures.displaySize}</div>
+        <div class=" border bg-light px-2">${
+          data?.mainFeatures?.displaySize
+        }</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -111,7 +140,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">memory</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.mainFeatures.memory}</div>
+        <div class=" border bg-light px-2">${data?.mainFeatures?.memory}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -119,7 +148,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">storage</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.mainFeatures.storage}</div>
+        <div class=" border bg-light px-2">${data?.mainFeatures?.storage}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -132,7 +161,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">Bluetooth</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.others.Bluetooth}</div>
+        <div class=" border bg-light px-2">${data?.others?.Bluetooth}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -140,7 +169,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">GPS</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.others.GPS}</div>
+        <div class=" border bg-light px-2">${data?.others?.GPS}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -148,7 +177,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">NFC</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.others.NFC}</div>
+        <div class=" border bg-light px-2">${data?.others?.NFC}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -156,7 +185,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">Radio</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.others.Radio}</div>
+        <div class=" border bg-light px-2">${data?.others?.Radio}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -164,7 +193,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">USB</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.others.USB}</div>
+        <div class=" border bg-light px-2">${data?.others?.USB}</div>
       </div>
     </div>
     <div class="row gx-0">
@@ -172,7 +201,7 @@ const displayPhoneDetails = (data) => {
       <div class="border bg-light px-2">WLAN</div>
       </div>
       <div class="col-8">
-        <div class=" border bg-light px-2">${data.others.WLAN}</div>
+        <div class=" border bg-light px-2">${data?.others?.WLAN}</div>
       </div>
     </div>
 </div>
